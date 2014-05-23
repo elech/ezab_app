ec82b5c142d8647d32de499bd9fdcf5694708f01
e <eric345@gmail.com>
2014-05-23 16:19:06 -0500

    flash message for new experiences

--- app/components/experiences/exp.add.ctrl.js
+++ app/components/experiences/exp.add.ctrl.js
@@ -1,11 +1,12 @@
 EZAB_APP.controller('experiencesAddCtrl',
-	['$scope', 'ExperienceService', '$stateParams', '$modalInstance', function($scope, ExperienceService, $stateParams, $modalInstance){
+	['$scope', 'ExperienceService', '$stateParams', '$modalInstance', 'flash', function($scope, ExperienceService, $stateParams, $modalInstance, flash){
 		$scope.tmp = {};
 		$scope.$modalInstance = $modalInstance;
 		$scope.createExperience = function(){
 			ExperienceService.createExperience($stateParams.propid, $stateParams.cid, $scope.tmp).then(function(res){
 				if(res.status === 201){
 					$modalInstance.close();
+					flash.success = "Created. Data will be available as it comes in"
 				}
 			})
 		}
